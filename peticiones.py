b81cbd3998a8e6229babe65c9e94fb9281dce7ec
unknown <fernando.ir@ix.agency>
2025-06-23 18:23:19 -0600

    orden en alarmas y control de archivos

--- peticiones.py
+++
@@ -1,5 +0,0 @@
-curl -X POST "http://127.0.0.1:5000/crear_alarma" -H "Content-Type: application/json" -d "{\"hora\": \"18:30\", \"audio\": \"C:/ruta/al/archivo.mp3\"}"
-
-curl -X GET "http://127.0.0.1:5000/consultar_alarmas"
-
-curl -X DELETE "http://127.0.0.1:5000/eliminar_alarma/1"
